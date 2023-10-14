fbde2a6d5843a2682f35d1148b7b2b7ee1c90d56
sindhupratapsingh <106824045+sindhupratapsingh@users.noreply.github.com>
2023-10-14 13:00:09 +0530

    Add files via upload

---
+++ user/user_auth/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(unique=True)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+   
+    
+#     def __str__(self):
+#         return self.username
